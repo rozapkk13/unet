f772ebb1e7c9ac012447102e18ea59072b67a514
rozapkk13 <rozapaulkk@gmail.com>
2025-02-23 21:24:43 +0530

    Initial commit - Added U-Net model

--- unet1_model.py
+++ unet1_model.py
@@ -3,19 +3,18 @@ os.environ["SM_FRAMEWORK"] = "tf.keras"
 import segmentation_models as sm
 from tensorflow.keras.optimizers import Adam
 
-def unet_model(input_size=(256, 256, 3), pretrained_weights=None):
+def unet(input_size=(256, 256, 3), pretrained_weights=None):
     """
     Load a pretrained U-Net model with a ResNet34 encoder.
     """
-
     # Load pretrained U-Net with ResNet34 as the encoder
     model = sm.Unet('resnet34', encoder_weights='imagenet', input_shape=input_size)
 
     # Compile model with binary crossentropy loss
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+   model.compile(optimizer=Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
     # Load pretrained weights if provided
     if pretrained_weights:
         model.load_weights(pretrained_weights)
 
-    return model
+    return model  # This should only be here once!
